28c99c3426dd9f5cfd289e19927459235674ae72
AnaVirginia12 <aarias20495@ufide.ac.cr>
2025-05-28 20:49:10 -0600

    PRACTICA EVALUADA ANA VIRGINA

---
+++ PractivaEvaluada1/src/practivaevaluada1/PractivaEvaluada1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practivaevaluada1;
+
+/**
+ *
+ * @author Ana Virginia
+ */
+public class PractivaEvaluada1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
